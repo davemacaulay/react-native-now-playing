0daa8bd64b4e09881fac029775d868e22c6784b8
Dave Macaulay <macaulay@adobe.com>
2019-07-07 11:07:18 -0500

    Remove deezer and spotify, update to support latest React Native

--- index.js
+++ index.js
@@ -1,25 +1,8 @@
-import { NativeEventEmitter, NativeModules, Platform } from 'react-native';
+import { NativeEventEmitter, NativeModules } from 'react-native';
 
 const RNNowPlaying = NativeModules.RNNowPlaying;
 const NowPlayingEventEmitter = new NativeEventEmitter(RNNowPlaying);
 
-const fetchDeezerData = (deezerUserId) =>
-	fetch("http://www.deezer.com/profile/" + deezerUserId)
-		.then(response => {
-			var html = response.text()["_55"];
-			var regexp = new RegExp(/{("online":{).*?(?=,"ALB_PICTURE")/g);
-			let res = regexp.exec(html)[0] + "}}";
-			res = JSON.parse(res);
-			return {
-				title: res.online.SNG_TITLE,
-				artist: res.online.ART_NAME,
-				albumTitle: res.online.ALB_TITLE
-			};
-		})
-		.catch(error => {
-			console.log("API getDeezer error:" + error);
-		});
-		
 class NowPlaying {
 	/**
 	Begin observing for music events
@@ -39,64 +22,12 @@ class NowPlaying {
 			"NowPlayingEvent",
 			this.eventCallback
 		);
-		if (Platform.OS == "ios")
-			this.authDeezer();
 	}
 	
 	stopObserving() {
 		console.log("NowPlaying stopObserving");
 		this.listener.remove();
-		//TODO RNNowPlaying.stopObserving();
-		if (Platform.OS == "ios")
-			clearInterval(this.deezerInterval);
 	}
-	
-	//////////////////////////////////////////////////////////
-	// DEEZER: for iOS only									//
-	//////////////////////////////////////////////////////////
-	
-	/**
-	Auth with deezer and retrieve deezer User Id
-	Then call fetchDeezerData every 60sec
-	**/
-	authDeezer(){
-		console.log("NowPlaying authDeezer");
-		// Tester si token existe
-		const APP_ID = "215504"; // 
-		const REDIRECT_URI = "";
-		const authUrl = "https://connect.deezer.com/oauth/auth.php?app_id="+APP_ID+"&redirect_uri="+REDIRECT_URI+"&perms=basic_access,offline_access&response_type=token";
-		// Fetch will open browser and then redirect user to the app
-		fetch(authUrl)
-			.then(response => {
-
-			})
-			.catch(error => {
-				console.log("Auth Deezer error:" + error);
-			});
-		
-
-		this.deezerUserId = "";
-		this.deezerInterval = setInterval(fetchDeezerData, 60000);
-	}
-	
-	
-	authDeezerCallback(){
-		//save token
-		console.log(response);
-		token = ""; // save token as it never expire
-		fetch("https://api.deezer.com/user/me?token"+token)
-			.then(response => {
-				console.log(response);
-				user = "";
-			})
-	}
-	
-	fetchDeezerData(){
-		console.log("NowPlaying fetchDeezerData");
-		var deezerNowPlaying = fetchDeezerData(this.deezerUserId);
-		this.eventCallback(deezerNowPlaying.title, deezerNowPlaying.albumTitle, deezerNowPlaying.artist, "iOS", "Deezer");
-	}
-	
 }
 
 export default new NowPlaying();
--- ios/RNNowPlaying.m
+++ ios/RNNowPlaying.m
@@ -26,23 +26,18 @@ static RNNowPlaying *RNNowPlayingInstance = nil;
 /**
  Register to events
  **/
--(void)startObserving{
+RCT_EXPORT_METHOD(startObserving){
     NSLog(@"RNNowPlaying startObserving");
     // Check Authorization on MPMediaLibrary
-    // Traiter les cas du refus...
-    if ([MPMediaLibrary authorizationStatus] == MPMediaLibraryAuthorizationStatusAuthorized){
-        [self registerNotif];
-    } else {
-        [MPMediaLibrary requestAuthorization:^(MPMediaLibraryAuthorizationStatus status) {
+    if (@available(iOS 9.3, *)) {
+        if ([MPMediaLibrary authorizationStatus] == MPMediaLibraryAuthorizationStatusAuthorized){
             [self registerNotif];
-        }];
+        } else {
+            [MPMediaLibrary requestAuthorization:^(MPMediaLibraryAuthorizationStatus status) {
+                [self registerNotif];
+            }];
+        }
     }
-    
-#if __has_include(<SpotifyAppRemoteSDK/SpotifyAppRemote.h>)
-    NSLog(@"Spotify Framework is installed !");
-    [self startObservingSpotify];
-#endif
-
 }
 
 -(void)registerNotif{
@@ -58,7 +53,6 @@ static RNNowPlaying *RNNowPlayingInstance = nil;
     //e.playbackTime, e.playbackDuration, e.title, e.albumTitle, e.artist
     MPMusicPlayerController *player = (MPMusicPlayerController *)notification.object;
     MPMediaItem *item = [player nowPlayingItem];
-
 
     NSString *playbackTime = [NSString stringWithFormat:@"%@", [NSNumber numberWithDouble:player.currentPlaybackTime]];
     NSString *playbackDuration = [NSString stringWithFormat:@"%@", [NSNumber numberWithDouble:item.playbackDuration]];
@@ -87,88 +81,4 @@ static RNNowPlaying *RNNowPlayingInstance = nil;
 
 }
 
-
-#if __has_include(<SpotifyAppRemoteSDK/SpotifyAppRemote.h>)
--(void)startObservingSpotify{
-    SPTAppRemoteConnectionParamsImageFormat format = SPTAppRemoteConnectionParamsImageFormatAny;
-    SPTAppRemoteConnectionParams *params =
-    [[SPTAppRemoteConnectionParams alloc] initWithClientIdentifier:@"e5dec3e3ce1643909db47c3d396861c8"
-                                                       redirectURI:@"spotifywatchexample://"
-                                                              name:@"Kizbee"
-                                                       accessToken:nil
-                                                  defaultImageSize:CGSizeZero
-                                                       imageFormat:format];
-    
-    self.appRemote = [[SPTAppRemote alloc] initWithConnectionParameters:params
-                                                               logLevel:SPTAppRemoteLogLevelDebug];
-    self.appRemote.delegate = self;
-    
-    [self.appRemote connect];
-    
-    [self startObserving];
-}
-
-
-- (void)appRemote:(SPTAppRemote *)appRemote didFailConnectionAttemptWithError:(NSError *)error
-{
-    // Connection failed
-    NSLog(@"Connection failed %@", error);
-    BOOL spotifyInstalled = [self.appRemote authorizeAndPlayURI:@""];
-    if (!spotifyInstalled) {
-        /*
-         * The Spotify app is not installed.
-         * Use SKStoreProductViewController with [SPTAppRemote spotifyItunesItemIdentifier] to present the user
-         * with a way to install the Spotify app.
-         */
-    }
-}
-
-- (void)appRemote:(SPTAppRemote *)appRemote didDisconnectWithError:(nullable NSError *)error
-{
-    // Connection disconnected
-    NSLog(@"Connection disconnected %@", error);
-}
-
-- (void)playerStateDidChange:(id<SPTAppRemotePlayerState>)playerState
-{
-    if (![playerState.track.name isEqualToString:@"Spotify"] && !playerState.isPaused){
-        NSString *playbackTime = [NSString stringWithFormat:@"%@", [NSNumber numberWithDouble:playerState.playbackPosition]];
-        NSString *playbackDuration = [NSString stringWithFormat:@"%@", [NSNumber numberWithDouble:playerState.track.duration]];
-        NSString *artist = [NSString stringWithFormat:@"%@", playerState.track.artist.name];
-        NSString *albumTitle = [NSString stringWithFormat:@"%@", playerState.track.album.name];
-        NSString *title = [NSString stringWithFormat:@"%@", playerState.track.name];
-        
-        NSLog(@"[Nowplaying] artist: %@", artist);
-        NSLog(@"[Nowplaying] albumTitle: %@", albumTitle);
-        NSLog(@"[Nowplaying] title: %@", title);
-    }
-    
-}
-
-- (void)appRemoteDidEstablishConnection:(SPTAppRemote *)appRemote
-{
-    // Connection was successful, you can begin issuing commands
-    
-    appRemote.playerAPI.delegate = self;
-    [appRemote.playerAPI subscribeToPlayerState:
-     ^(id  _Nullable result, NSError * _Nullable error) {
-         if (error) {
-             NSLog(@"%@", error.localizedDescription);
-         }
-     }];
-}
-
-- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options
-{
-    NSDictionary *params = [self.appRemote authorizationParametersFromURL:url];
-    NSString *token = params[SPTAppRemoteAccessTokenKey];
-    if (token) {
-        self.appRemote.connectionParameters.accessToken = token;
-    } else if (params[SPTAppRemoteErrorDescriptionKey]) {
-        NSLog(@"%@", params[SPTAppRemoteErrorDescriptionKey]);
-    }
-    return YES;
-}
-#endif
-    
 @end
